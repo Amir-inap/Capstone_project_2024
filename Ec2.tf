f6d456c0b74913bc27228ce45142600dc57ce458
Amir-inap <amirhossein.aslian@gmail.com>
2024-07-17 14:12:53 +0200

    Added Amazon_Linux_2 as AMI and using LAMP userscript on my EC2

---
+++ Ec2.tf
@@ -0,0 +1,41 @@
+locals {
+  name = "WordPress Instance ${var.tagNameDate}"
+}
+#Get latest ami ID of Amazon Linux _2- values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+#data "aws_ami" "amazon_linux" {
+  #most_recent = true
+  #owners      = ["amazon"]
+
+  #filter {
+    #name   = "name"
+    #values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+ # }
+#}
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+
+resource "aws_instance" "wordpress_instance" {
+  ami                         = data.aws_ami.amazon_linux_2.id
+  instance_type               = var.ec2_instance_type
+  availability_zone           = var.availability_zones[0]
+  key_name                    = var.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.wordpress_sg.id]
+  subnet_id                   = aws_subnet.public[0].id # Choose one of the public subnets
+
+
+  tags = {
+    Name = local.name
+  }
+  user_data = file("UserDataEC2.sh")
+  #user_data = data.template_file.userdataEC.rendered
+
+
+}
