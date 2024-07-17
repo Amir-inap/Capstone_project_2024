42b3a5046893c2763cbf4f7b53cc81fb734986d0
Amir-inap <amirhossein.aslian@gmail.com>
2024-07-17 14:30:11 +0200

    Using LAMP for develop and host my web application on Ec2

---
+++ UserDataEC2.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Install updates
+sudo yum update -y
+
+# Install Apache server
+sudo yum install -y httpd
+
+# Install MariaDB, PHP, and necessary tools
+sudo amazon-linux-extras install -y php8.0
+sudo yum install -y mariadb-server unzip
+
+# Set database variables
+DBName="wordpress"
+DBUser="wordpress"
+DBPassword="admin1234"
+DBRootPassword="rootpassword1234"
+DBHost="localhost"
+
+# Start Apache server and enable it on system startup
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+# Start MariaDB service and enable it on system startup
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+
+# Wait for MariaDB to fully start
+sleep 10
+
+# Set MariaDB root password
+sudo mysqladmin -u root password "$DBRootPassword"
+
+# Download and install WordPress
+sudo wget http://wordpress.org/latest.tar.gz -P /var/www/html
+cd /var/www/html
+sudo tar -zxvf latest.tar.gz
+sudo cp -rvf wordpress/* .
+sudo rm -R wordpress
+sudo rm latest.tar.gz
+
+# Making changes to the wp-config.php file, setting the DB name
+sudo cp ./wp-config-sample.php ./wp-config.php 
+sudo sed -i "s/'database_name_here'/'$DBName'/g" wp-config.php
+sudo sed -i "s/'username_here'/'$DBUser'/g" wp-config.php
+sudo sed -i "s/'password_here'/'$DBPassword'/g" wp-config.php
+sudo sed -i "s/'localhost'/'$DBHost'/g" wp-config.php
+
+# Grant permissions
+sudo usermod -a -G apache ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www
+sudo find /var/www -type d -exec chmod 2775 {} \;
+sudo find /var/www -type f -exec chmod 0664 {} \;
+
+# Create WordPress database
+#echo "CREATE DATABASE IF NOT EXISTS $DBName;" | mysql -u root --password=$DBRootPassword
+echo "CREATE DATABASE $DBName;" | mysql -u root --password=$DBRootPassword
+#echo "CREATE USER IF NOT EXISTS '$DBUser'@'localhost' IDENTIFIED BY '$DBPassword';" | mysql -u root --password=$DBRootPassword
+echo "CREATE USER '$DBUser'@'localhost' IDENTIFIED BY '$DBPassword';" | mysql -u root --password=$DBRootPassword
+echo "GRANT ALL ON $DBName.* TO '$DBUser'@'localhost';" | mysql -u root --password=$DBRootPassword
+echo "FLUSH PRIVILEGES;" | mysql -u root --password=$DBRootPassword
+
+# Update the database with the new site URL
+IP_ADDRESS=$(curl -s http://checkip.amazonaws.com)
+mysql -u root -p"$DBRootPassword" $DBName -e "UPDATE wp_options SET option_value = 'http://$IP_ADDRESS' WHERE option_name = 'siteurl' OR option_name = 'home';"
+
+
